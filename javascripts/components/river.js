976710cf3780768e000227df6fae82006c4669f1
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-03-01 21:59:59 -0600

    added some styling and new data

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -3,11 +3,11 @@ import util from '../helpers/util.js';
 const bearCardBuilder = (arr) => {
     let domString = '';
     for(let i = 0; i < arr.length; i++) {
-        domString += '<div class="card">';
-        domString += `<img src="${arr[i].url}" class="card-img-top" alt="...">`;
+        domString += '<div class="card col-3 m-1">';
+        domString += `<img src="${arr[i].url}" class="card-img-top mt-2" alt="...">`;
         domString += '<div class="card-body">';
         domString += `<h5 class="card-title">${arr[i].name}</h5>`;
-        domString += '<a href="#" class="btn btn-primary">Go somewhere</a>';
+        domString += '<a href="#" class="btn btn-primary">More Info</a>';
         domString += '</div>';
         domString += '</div>';
     }
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,11 @@
 import bearForm from './components/bearForm.js';
+import river from './components/river.js';
+import bearData from './helpers/data/bearData.js';
 
 const init = () => {
+    const bears = bearData.getBears();
     bearForm.events();
+    river.bearCardBuilder(bears);
 };
 
 init();
